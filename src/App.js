e500c56dc72b7c00a471f0e6e2fd27a47987ffe9
= <=>
2023-05-18 01:28:47 +0100

    created the routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Profile from "./pages/Profile";
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-        <h1 className='text-xl'>Hello</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/forgotPassword' element={<ForgotPassword />} />
+      </Routes>
+    </Router>
   );
 }
 
